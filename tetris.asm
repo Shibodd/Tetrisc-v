12dd3912f7c38f6e90be559c529b896445f0bbf1
Simone Bondi <32235177+Shibodd@users.noreply.github.com>
2021-05-11 23:30:58 +0200

    Finished the blit procedure.

--- tetris.asm
+++ tetris.asm
@@ -63,15 +63,14 @@ falling_tetromino_c: .word 5
 .text
 main:
 	call clear_board
-
-	## begin blit
-	li a0, 0 # matrix addr
-	li a1, 3 # matrix width
+	
+	la a0, board # matrix addr
+	li a1, 10 # matrix width
 	li a2, 0 # draw box row
 	li a3, 0 # draw box column
-	li a4, 2 # draw box width
-	li a5, 2 # draw box height
-	li a6, 1 # row
+	li a4, 10 # draw box width
+	li a5, 20 # draw box height
+	li a6, 0 # row
 	li a7, 1 # column
 	call blit
 	
@@ -90,15 +89,18 @@ clear_board:
 clear_board_loop:
 	sw t3, 0(t0)
 	addi t0, t0, 4
-	bne t0, t1, clear_board_loop
+	blt t0, t1, clear_board_loop
 	jr ra
 ## end clear_board
 
 
 ## begin blit
+# Blits a portion of a matrix at a position on the screen.
+# Colors are picked from colors based on the value in the matrix cell.
+# 
 # (in) a0: matrix base address
 # (in) a1: matrix width (in power of 2 form)
-# draw box: subset of matrix to draw
+# draw box: portion of matrix to draw
 # (in) a2: draw box row
 # (in) a3: draw box column
 # (in) a4: draw box width
@@ -107,51 +109,59 @@ clear_board_loop:
 # (in) a6: row
 # (in) a7: column
 blit:
-	## t0 = screen row base address
-	la t0, screen  # screen_addr = screen base address
-	slli t1, a6, 7 # row_offset = row * 32 * 4
-	add t0, t0, t1 # screen_addr += row_offset
-	slli t1, a7, 2 # column_offset = column * 4
-	add t0, t0, t1 # screen_addr += column_offset
+	## t0 = screen address
+	la t0, screen	# screen_addr = screen base address
+	slli t1, a6, 7	# row_offset = row * 32 * 4
+	add t0, t0, t1	# screen_addr += row_offset
+	slli t1, a7, 2	# column_offset = column * 4
+	add t0, t0, t1	# screen_addr += column_offset
 	
-	## a0 = matrix row base address
-	sll t1, a2, a1 # row_offset = row * matrix width
-	add a0, a0, t1 # mat_addr += row_offset
-	add a0, a0, a3 # mat_addr += column
+	## a0 = matrix address
+	sll t1, a2, a1	# row_offset = row * matrix width
+	add a0, a0, t1	# mat_addr += row_offset
+	add a0, a0, a3	# mat_addr += column
 	
-	# t5 = matrix row size
-	li t5, 1 # matrix row size = 1
-	sll t5, t5, a1 # matrix row size = 2 ^ matrix width (in power of 2 form)
+	## t1 = screen newline offset = (32 - a4) * 4
+	li t1, 32
+	sub t1, t1, a4
+	slli t1, t1, 2
 	
-	# t1 = screen draw address
-	# t2 = matrix read address
-	# t3 = row index
-	# t4 = column index
+	## a1 = matrix newline offset = (mat_width - draw box width) * 4
+	sub a1, a1, a4
+	slli a1, a1, 2
+	
+	# t2 = row index
+	# t3 = column index
 	
 	# for (t3 = 0; t3 < a5; ++t3)
-	li t3, 0
+	li t2, 0
 blit_row_loop:
-	bge t3, a5, blit_row_loop_end
-	mv t1, t0 # screen draw address = screen row base address
-	mv t2, a0 # matrix read address = matrix row base address
+	bge t2, a5, blit_row_loop_end
 	
 	# for (t4 = 0; t4 < a4; ++t4)
-	li t4, 0
+	li t3, 0
 blit_column_loop:
-	bge t4, a4, blit_column_loop_end
+	bge t3, a4, blit_column_loop_end
+
 	
-	li t5, 0xFFFFFF
-	sw t5, 0(t1)
+	# Load the color
+	lb t4, 0(a0)
+	la t5, colors
+	slli t4, t4, 2
+	add t5, t5, t4
+	lw t4, 0(t5)
+	sw t4, 0(t0)
+
 	
-	addi t1, t1, 4 # screen draw address += 4
-	addi t2, t2, 1 # matrix read address += 1
-	addi t4, t4, 1
+	addi t0, t0, 4 # screen draw address += 4
+	addi a0, a0, 1 # matrix read address += 1
+	addi t3, t3, 1 # ++col
 	j blit_column_loop
 	
 blit_column_loop_end:
-	addi t3, t3, 1
-	addi t0, t0, 128 # screen row base address += 32 * 4 (newline)
-	add a0, a0, t5 # matrix row base address += matrix row size (newline)
+	addi t2, t2, 1 # ++row
+	add t0, t0, t1 # screen address += screen newline offset
+	add a0, a0, a1 # matrix address += matrix newline offset
 	
 	j blit_row_loop
 blit_row_loop_end:
