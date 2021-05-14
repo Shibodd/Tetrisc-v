64f249709358103580608cb47cc5edeee56e4555
Shibodd <simonebondi456@gmail.com>
2021-05-14 11:55:50 +0200

    Added a rotation direction parameter to rotate_tetromino. Swapped the source/dest arguments in memcpy.

--- matrices.asm
+++ matrices.asm
@@ -1,16 +1,16 @@
 ## begin memcpy
 # Copies a memory portion.
-# (in) a0: source matrix address
-# (in) a1: destination matrix address
+# (in) a0: destination matrix address
+# (in) a1: source matrix address
 # (in) a2: bytes to copy
 memcpy:
 	bge zero, a2, memcpy_end # a2 <= 0  <=>  0 >= a2
 	
-	lb t0, 0(a0) # tmp = *src_addr
-	sb t0, 0(a1) # *dst_addr = tmp
+	lb t0, 0(a1) # tmp = *src_addr
+	sb t0, 0(a0) # *dst_addr = tmp
 	
-	addi a0, a0, 1 # src_addr ++
-	addi a1, a1, 1 # dst_addr ++
+	addi a1, a1, 1 # src_addr ++
+	addi a0, a0, 1 # dst_addr ++
 	addi a2, a2, -1
 	j memcpy
 memcpy_end:
--- tetris.asm
+++ tetris.asm
@@ -78,10 +78,59 @@ main:
 	li a4, 0xAAAAAA
 	call draw_box
 	
+	
+	call test
+	
+	
 	li a0, 0
 	li a7, 93
 	ecall 
 ## end main
+
+
+## begin test
+test:
+	addi sp, sp, -8
+	sw ra, 0(sp)
+	sw s0, 4(sp)
+
+	# set falling tetromino type
+	la t0, falling_tetromino_type
+	li t1, 2
+	sb t1, 0(t0)
+	mv s0, t1 # s0 = tetromino type
+	
+	la a0, falling_tetromino_matrix
+	la a1, tetromino_matrices
+	mv t1, s0
+	addi t1, t1, -1
+	slli t1, t1, 4
+	add a1, a1, t1
+	li a2, 16
+	call memcpy
+
+	li a0, -1
+	call rotate_tetromino
+	
+	call get_tetromino_matrix_size
+	mv a2, a0 # (in) a2: draw box width
+	mv a3, a0 # (in) a3: draw box height
+	
+	li a0, 10 # (in) a0: row
+	li a1, 16 # (in) a1: column
+	li a4, 0 # (in) a4: draw box row
+	li a5, 0 # (in) a5: draw box column
+	la a6, falling_tetromino_matrix # (in) a6: matrix address
+	li a7, 4 # (in) a7: matrix width
+	call blit
+	
+
+	lw ra, 0(sp)
+	lw s0, 4(sp)
+	addi sp, sp, 8
+	
+	ret
+## end test
 
 
 
@@ -101,37 +150,36 @@ get_tetromino_matrix_size_typeI:
 
 
 
-## begin rotate_tetromino_right
-rotate_tetromino_right:
+## begin rotate_tetromino
+# (in) a0: direction (if a0 >= 0 then right, else left)
+rotate_tetromino:
 	lb t0, falling_tetromino_type
 	addi t0, t0, -4
-	bne t0, zero, rotate_tetromino_right_ok # Return if falling_tetromino_type = 4
+	bne t0, zero, rotate_tetromino_ok # Return if falling_tetromino_type = 4
 	ret
-rotate_tetromino_right_ok:
-	addi sp, sp, -20
+rotate_tetromino_ok:
+	addi sp, sp, -24
 	sw ra, 0(sp)
 	sw s0, 4(sp)
 	sw s1, 8(sp)
 	sw s2, 12(sp)
 	sw s3, 16(sp)
+	sw s4, 20(sp)
 	
+	mv s4, a0 # s4 = rotation direction
 	
 	call get_tetromino_matrix_size
-	
-	li a7, 1
-	ecall
-	
 	
 	mv s2, a0 # t2 = tetromino matrix size
 	
 	li a2, 2 # width argument of square_matrix_index_at
 
-	li s0, 0 # t0 = row
-rotate_tetromino_right_row_loop:
-	bge s0, s2, rotate_tetromino_right_row_loop_end
-	li s1, 0 # t1 = column
-rotate_tetromino_right_column_loop:
-	bge s1, s2, rotate_tetromino_right_column_loop_end
+	li s0, 0 # s0 = row
+rotate_tetromino_row_loop:
+	bge s0, s2, rotate_tetromino_row_loop_end
+	li s1, 0 # s1 = column
+rotate_tetromino_column_loop:
+	bge s1, s2, rotate_tetromino_column_loop_end
 	
 	# new column = tetromino matrix size - 1 - row
 	
@@ -148,39 +196,57 @@ rotate_tetromino_right_column_loop:
 	add s3, s3, a0 
 	# s3 = src address
 	
+	
+	# a0 = new row
+	# a1 = new column
+	bge s4, zero, rotate_tetromino_right_calc
+rotate_tetromino_left_calc:	
+	# new row = tetromino matrix size - 1 - column
+	# new column = row
+	mv a1, s0 # new column = row
+	addi a0, s2, -1 # new row = tetromino matrix size - 1
+	sub a0, a0, s1 # new row -= column
+	
+	j rotate_tetromino_calc_end
+rotate_tetromino_right_calc:	
+	# new row = column
+	# new column = tetromino matrix size - 1 - row
 	mv a0, s1 # new row = column
-	## new column = tetromino matrix size - 1 - row
 	addi a1, s2, -1 # new column = tetromino matrix size - 1
 	sub a1, a1, s0 # new column -= row
+	
+rotate_tetromino_calc_end:
 	call square_matrix_index_at 
 	la t4, matrix_rotation_temp
 	add a0, a0, t4 
 	# a0 = dst address
-	
+
 	lb s3, 0(s3)
 	sb s3, 0(a0)
 
 	addi s1, s1, 1 # column++
-	j rotate_tetromino_right_column_loop
-rotate_tetromino_right_column_loop_end:
+	j rotate_tetromino_column_loop
+rotate_tetromino_column_loop_end:
 	addi s0, s0, 1 # row++
-	j rotate_tetromino_right_row_loop
-rotate_tetromino_right_row_loop_end:
+	j rotate_tetromino_row_loop
+rotate_tetromino_row_loop_end:
 	
-	la a0, matrix_rotation_temp
-	la a1, falling_tetromino_matrix
+	la a0, falling_tetromino_matrix
+	la a1, matrix_rotation_temp
 	li a2, 16
 	call memcpy
 
+	
 	lw ra, 0(sp)
 	lw s0, 4(sp)
 	lw s1, 8(sp)
 	lw s2, 12(sp)
 	lw s3, 16(sp)
-	addi sp, sp, 20
+	lw s4, 20(sp)
+	addi sp, sp, 24
 	ret
 	
-## end rotate_tetromino_right
+## end rotate_tetromino
 
 .include "matrices.asm"
 .include "drawing.asm"
